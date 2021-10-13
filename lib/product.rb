b78d0a3d6d690feb966e376b970f7deab7a386b2
Tatsiana Novikava <tatsiana.novikava.other@gmail.com>
2021-10-13 18:48:02 +0300

    use hash

--- lib/product.rb
+++ lib/product.rb
@@ -1,8 +1,8 @@
 class Product
   attr_reader :price, :stock
 
-  def initialize(price, stock)
-    @price = price
-    @stock = stock
+  def initialize(product_attributes)
+    @price = product_attributes[:price]
+    @stock = product_attributes[:stock]
   end
 end
--- main.rb
+++ main.rb
@@ -2,6 +2,6 @@ require_relative 'lib/product'
 require_relative 'lib/book'
 require_relative 'lib/film'
 
-leon_film = Film.new(1500,30)
+leon_film = Film.new(price: 1500, stock: 30)
 
 puts "Фильм Леон стоит #{leon_film.price} руб."
